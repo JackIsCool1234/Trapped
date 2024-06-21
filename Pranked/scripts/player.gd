e840d87d08d2ae2c950dcd8bc988589c2ad433de
JackIsCool1234 <jwaa@students.mags.school.nz>
2024-06-21 12:31:09 +1200

    player

--- Pranked/scripts/player.gd
+++ Pranked/scripts/player.gd
@@ -5,12 +5,19 @@ var distance = Vector2()
 
 @export var speed = 250
 
+var direction = 0
+
 func _ready():
 	destination = position
 
-func _process():
-	
+func _process(delta):
+	if global_position.distance_to(destination) > 50:
+		velocity.x = speed * direction
 
 func _input(event):
 	if Input.is_action_pressed("ui_leftMouseClick"):
 		destination = get_global_mouse_position()
+		if destination < global_position:
+			direction = -1
+		else:
+			direction = 1
